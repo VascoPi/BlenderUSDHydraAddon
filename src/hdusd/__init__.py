130b0345b6e886936e7c404dd1184177089c586d
Vasyl Pidhirskyi <sdilmob@gmail.com>
2022-05-03 23:01:32 +0300

    BLEN-32: Move some USD Hydra options to plugin preferences.

--- src/hdusd/__init__.py
+++ src/hdusd/__init__.py
@@ -28,8 +28,65 @@ bl_info = {
 version_build = ""
 
 
+import tempfile
+from pathlib import Path
+
 from . import config
-from .utils import logging
+from .utils import logging, temp_dir
+
+import bpy
+from bpy.types import Operator, AddonPreferences
+from bpy.props import StringProperty, IntProperty, BoolProperty, EnumProperty
+
+
+class UsdAddonPreferences(AddonPreferences):
+    bl_idname = __name__
+
+    def update_temp_dir(self, context):
+        if tempfile.gettempdir() == str(Path(self.tmp_dir)):
+            log.info(f"Current temp directory is {self.tmp_dir}")
+            return
+
+        if not Path(self.tmp_dir).exists():
+            return
+
+        tempfile.tempdir = Path(self.tmp_dir)
+        log.info(f"Current temp directory is changed to {self.tmp_dir}")
+
+    def update_dev_tools(self, context):
+        config.show_dev_settings = self.dev_tools
+        log.info(f"Developer settings is {'enabled' if self.dev_tools else 'disabled'}")
+
+    def update_debug_log(self, context):
+        config.logging_level = 'DEBUG' if self.debug_log else 'INFO'
+        log.info(f"Log level 'DEBUG' is {'enabled' if self.debug_log else 'disabled'}")
+
+    tmp_dir: StringProperty(
+        name="Temp Directory",
+        description="Set temp directory",
+        maxlen=1024,
+        subtype='DIR_PATH',
+        default=str(temp_dir().parent),
+        update=update_temp_dir,
+    )
+    dev_tools: BoolProperty(
+        name="Developer Tools",
+        description="Enable developer tools",
+        default=config.show_dev_settings,
+        update=update_dev_tools,
+    )
+    debug_log: BoolProperty(
+        name="Debug",
+        description="Enable debug console output",
+        default= config.logging_level != 'INFO',
+        update=update_debug_log,
+    )
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, "tmp_dir", icon='NONE' if Path(self.tmp_dir).exists() else 'ERROR')
+        layout.prop(self, "dev_tools")
+        layout.prop(self, "debug_log")
+
 
 log = logging.Log('init')
 log.info(f"Loading USD Hydra addon version={bl_info['version']}, build={version_build}")
@@ -41,6 +98,7 @@ def register():
     """ Register all addon classes in Blender """
     log("register")
 
+    bpy.utils.register_class(UsdAddonPreferences)
     engine.register()
     bl_nodes.register()
     mx_nodes.register()
@@ -59,3 +117,4 @@ def unregister():
     ui.unregister()
     properties.unregister()
     engine.unregister()
+    bpy.utils.unregister_class(UsdAddonPreferences)
